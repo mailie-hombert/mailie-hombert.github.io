2ea940cb442a39e87c2586df6abdb75ad6f71104
mailiehombert <67342315+mailiehombert@users.noreply.github.com>
2021-09-19 15:38:58 +0200

    inr

---
+++ INR_mailie/articles.php
@@ -0,0 +1,136 @@
+<!-- 
+  &#160; = espace insécable (empèche le passage à la ligne)
+
+  &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
+
+  tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+-->
+<head>
+<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+
+<section>
+      
+  <aside>    
+    <h2>
+    <a href="https://mailie-hombert.github.io/" target="_blank"> Maïlie HOMBERT</a>
+    </h2>
+    <p>
+      La réalité virtuelle
+    </p>
+  </aside>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="Réalité virtuelle">
+    <h3>
+      CADOZ, Claude (auteur).  
+    </h3>
+
+    <h4>
+      <a href="https://www.decitre.fr/ebooks/les-realites-virtuelles-9782403027860_9782403027860_1.html#" target="_blank">
+        Les réalités virtuelle 
+      </a>
+    </h4> 
+    
+    <h5>
+      Flammarion, publié en 1994.
+    </h5>
+    
+    <blockquote cit="#">"L'image aujourd'hui n'est plus seulement cet objet plan fait uniquement pour les yeux ;
+    ce sont des objets que l'on peut toucher, manipuler,entendre, et qui résistent ou s'animent sous nos mains, c'est un espace dans lequel on peut entrer".</blockquote>
+    
+    <p> 
+    Ce livre décrit les techniques et le effets des réalités virtuelles et montre comment ces dernières permettent une immersion dans l'image.
+    </p>
+   
+
+    <h6> 
+    En cours de lecture 
+    </h6>
+  
+  </article>
+
+
+  <article class="filterDiv tag1 tag2">
+    <h3>
+      MORRIET, Oriane (auteur).  
+    </h3>
+
+    <h4>
+      <a href="https://journals.openedition.org/map/3825#article-3825" target="_blank">
+      Concevoir et écrire pour la réalité virtuelle : nouvelles compétences, approches et techniques de scénarisation
+      </a>
+    </h4> 
+    
+    <h5>
+      OpenEdition Journals, publié en 2019.
+    </h5>
+    
+    <blockquote cit="#">"Pour garder le contrôle sur l’histoire en train d’être narrée, les scénaristes attirent donc l’attention de l’utilisateur en utilisant tour à tour la proximité des objets du décor, l’action d’un personnage, la couleur, la lumière ou encore le son, 
+    comme le souligne Kath Dooley (2017, p. 169) dans son article « Storytelling with Virtual Reality in 360-Degrees: a New Screen Grammar" ».</blockquote>
+    
+    <p> 
+    Cette article fait un bilan des nouvelles compétences, approches et techniques de scénarisation 
+    des scénaristes des œuvres en réalité virtuelle afin de mieux saisir ce que signifie concevoir et écrire pour ce médium.
+    </p>
+   
+
+    <h6> 
+      Consulté le 15/09/2021 
+    </h6>
+  
+  </article>
+
+
+
+  <article class="filterDiv tag1 tag2">
+    <h3>
+      NOM, Prénom (auteur).  
+    </h3>
+
+    <h4>
+      <a href="#" target="_blank">
+        Titre, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Éditeur, date de publication.
+    </h5>
+    
+    <blockquote cit="#">Citation</blockquote>
+    
+    <p> 
+      Résumé
+    </p>
+   
+
+    <h6> 
+      Date de lecture / consultation  
+    </h6>
+  
+  </article>
+
+
+
+
+
+</section>
+
+
+
+
