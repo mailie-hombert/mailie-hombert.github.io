796591f0f550e79402ddc4b922070197b0b7a23f
mailiehombert <67342315+mailiehombert@users.noreply.github.com>
2021-10-11 14:59:24 +0200

    changement dans IRN

--- INR_mailie/articles.php
+++ INR_mailie/articles.php
@@ -22,7 +22,7 @@
     <a href="https://mailie-hombert.github.io/" target="_blank">portfolio</a>
     </h2>
     <p>
-     L'appropriation de la VR (Réalité Virtuelle) par les musées.
+     Mon thème de recherche porte sur l'utilisation de la réalité virtuelle (=VR) dans les musées.
     </p>
   </aside>
 
@@ -35,14 +35,14 @@
  /_/   \_\_|   \__|_|\___|_|\___|
  -->
 
-  <article class="filterDiv realitevirtuelle">
+  <article class="filterDiv realitevirtuelle image">
     <h3>
       CADOZ, Claude (auteur).  
     </h3>
 
     <h4>
       <a href="https://www.decitre.fr/ebooks/les-realites-virtuelles-9782403027860_9782403027860_1.html#" target="_blank">
-        Les réalités virtuelle 
+        Les réalités virtuelles 
       </a>
     </h4> 
     
@@ -50,8 +50,8 @@
       Flammarion, publié en 1994.
     </h5>
     
-    <blockquote cit="#">L'image aujourd'hui n'est plus seulement cet objet plan fait uniquement pour les yeux ;
-    ce sont des objets que l'on peut toucher, manipuler,entendre, et qui résistent ou s'animent sous nos mains, c'est un espace dans lequel on peut entrer>Citation</blockquote>
+    <blockquote cit="#">L'image aujourd'hui n'est plus seulement cet objet plan fait uniquement pour les yeux&#8239;;
+    ce sont des objets que l'on peut toucher, manipuler,entendre, et qui résistent ou s'animent sous nos mains, c'est un espace dans lequel on peut entrer.</blockquote>
     
     <p> 
     Ce livre décrit les techniques et le effets des réalités virtuelles et montre comment ces dernières permettent une immersion dans l'image.
@@ -59,20 +59,20 @@
    
 
     <h6> 
-    En cours de lecture 
+    lu 08 Octobre 2021
     </h6>
   
   </article>
 
 
-  <article class="filterDiv realitevirtuelle">
+  <article class="filterDiv realitevirtuelle scénarisation">
     <h3>
       MORRIET, Oriane (auteur).  
     </h3>
 
     <h4>
       <a href="https://journals.openedition.org/map/3825#article-3825" target="_blank">
-      Concevoir et écrire pour la réalité virtuelle : nouvelles compétences, approches et techniques de scénarisation
+      Concevoir et écrire pour la réalité virtuelle&#8239;: nouvelles compétences, approches et techniques de scénarisation.
       </a>
     </h4> 
     
@@ -81,7 +81,7 @@
     </h5>
     
     <blockquote cit="#">Pour garder le contrôle sur l’histoire en train d’être narrée, les scénaristes attirent donc l’attention de l’utilisateur en utilisant tour à tour la proximité des objets du décor, l’action d’un personnage, la couleur, la lumière ou encore le son, 
-    comme le souligne Kath Dooley (2017, p. 169) dans son article « Storytelling with Virtual Reality in 360-Degrees: a New Screen Grammar">Citation</blockquote>
+    comme le souligne Kath Dooley (2017, p. 169) dans son article "&#8239;Storytelling with Virtual Reality in 360-Degrees: a New Screen Grammar&#8239;"</blockquote>
     
     <p> 
     Cette article fait un bilan des nouvelles compétences, approches et techniques de scénarisation 
@@ -97,26 +97,26 @@
 
 
 
-  <article class="filterDiv realitevirtuelle experienceimmersive interview">
+  <article class="filterDiv realitevirtuelle experienceimmersive designfiction metaverse">
     <h3>
       FUTURMAG-ARTE  
     </h3>
 
     <h4>
       <a href="https://www.youtube.com/watch?v=guHt8dcCi-U" target="_blank">
-      Réalité virtuelle, vers l’immersion totale ? 
+      Réalité virtuelle, vers l’immersion totale&#8239;? 
       </a>
     </h4> 
     
     <h5>
-      ARTE, publié le 28 novembre 2016
+      ARTE, publié le 28 novembre 2016.
     </h5>
     
     <blockquote cit="#">Les mondes virtuels des mondes habitables de la même manière que notre monde réel.
-    Des gens iront travailler dans le monde virtuel ce sera leur vie ...>Citation</blockquote>
+    Des gens iront travailler dans le monde virtuel ce sera leur vie...</blockquote>
     
     <p> 
-      Vidéo de 20 minutes, expliquant les différentes avancées dans la VR en interviewant des spécialistes qui explique chacun leur point de vue sur la VR.
+      Il s'agit d'une vidéo de 20 minutes, expliquant les différentes avancées dans la VR en interviewant des spécialistes qui explique chacun leur point de vue sur la VR.
       Montrant les avantages que cette technique pourrait apporter mais les inconvénients qu'elle pourrait engendrer sur l'Homme.
     </p>
    
@@ -128,9 +128,9 @@
   </article>
 
 
-  <article class="filterDiv realitevirtuelle experienceimmersive">
+  <article class="filterDiv realitevirtuelle experienceimmersive metaverse">
     <h3>
-       
+       Auteur inconnu
     </h3>
 
     <h4>
@@ -140,15 +140,15 @@
     </h4> 
     
     <h5>
-      publié le 14 décembre 2018
+      publié le 14 décembre 2018.
     </h5>
     
-    <blockquote cit="#">Cette expérience vise à faire voyager le public dans l’espace et dans le temps>Citation</blockquote>
+    <blockquote cit="#">Cette expérience vise à faire voyager le public dans l’espace et dans le temps</blockquote>
     
     <p> 
-      Les musées d’Orsay et de l’Orangerie en partenariat avec la Mission du centenaire de la Première Guerre mondiale, 
+      Cette article nous parle des musées d’Orsay et de l’Orangerie en partenariat avec la Mission du centenaire de la Première Guerre mondiale, 
       et dans le cadre de l’année Clemenceau, ont organisés un focus Monet – Clemenceau qui a été présenté au musée de l’Orangerie du 12 novembre 2018 au 11 mars 2019. 
-      Cette exposition s’accompagnait d’une exploration de 7min en VR des Nympheas. 
+      Cette exposition s’accompagnait d’une exploration de 7 min en VR des Nympheas. 
     </p>
    
 
@@ -160,9 +160,9 @@
 
 
 
-  <article class="filterDiv realitevirtuelle ">
+  <article class="filterDiv realitevirtuelle metaverse musee interactif">
     <h3>
-      NORTHWAY Colin & STETHEM Robin (Fondateurs)
+      NORTHWAY, Colin & STETHEM, Robin (Fondateurs)
     </h3>
 
     <h4>
@@ -172,7 +172,7 @@
     </h4> 
     
     <h5>
-      Conçu en 2017
+      Conçu en 2017.
     </h5>
     
     <blockquote cit="#">Un nouvel espace pour un nouveau type de culture</blockquote>
@@ -191,7 +191,7 @@
 
   <article class="filterDiv realitevirtuelle">
     <h3>
-     GUERRIERO Emmanuel & BARATI Fabien (Fondateurs).  
+     GUERRIERO, Emmanuel & BARATI, Fabien (Fondateurs).  
     </h3>
 
     <h4>
@@ -201,14 +201,14 @@
     </h4> 
     
     <h5>
-      Fondée en 2005
+      Fondée en 2005.
     </h5>
     
-    <blockquote cit="#">La réalité virtuelle est l’opportunité de raconter une histoire d’une meilleure manière ,une histoire que l’on vit, pas que l’on consomme.</blockquote>
+    <blockquote cit="#">La réalité virtuelle est l’opportunité de raconter une histoire d’une meilleure manière, une histoire que l’on vit, pas que l’on consomme.</blockquote>
     
     <p> 
     Emissive est une agence qui crée des expériences immersives uniques en réalité virtuelle et en réalité augmentée. Fondée en 2005 par Emmanuel Guerriero et Fabien Barait,
-    deux passionnés de technologies immersives. Emissive est l'agence qui a conçu l'expérience VR "En tête-à-tête avec la Joconde", première expérience du musée du Louvre.
+    deux passionnés de technologies immersives. Emissive est l'agence qui a conçu l'expérience VR "&#8239;En tête-à-tête avec la Joconde&#8239;", première expérience du musée du Louvre.
     </p>
    
 
@@ -219,26 +219,26 @@
   </article>
 
 
-  <article class="filterDiv musee mediation">
+  <article class="filterDiv exprienceimmersive mediation musee">
     <h3>
-      NASSIM ABOUDRAR, Bruno & MAIRESSE,François (auteur).  
+      NASSIM ABOUDRAR, Bruno & MAIRESSE, François (auteur).  
     </h3>
 
     <h4>
-      <a href="#" target="_blank">
+      <a href="https://www.cairn.info/la-mediation-culturelle--9782130812166.htm" target="_blank">
         La médiation culturelle 
       </a>
     </h4> 
     
     <h5>
-    Édité par PUF (Presses Universitaires de France), publié en 2016
+    Édité par PUF (Presses Universitaires de France), publié en 2016.
     </h5>
     
-    <blockquote cit="#">La médiation se distingue alors de l'arbitrage, en ce que le médiateur, contrairement  à l'arbitre, 
+    <blockquote cit="#">La médiation se distingue alors de l'arbitrage, en ce que le médiateur, contrairement à l'arbitre, 
     n'impose pas ses vues aux parties, mais propose seulement des mesures équitables.</blockquote>
     
     <p> 
-      Ce livre traite de la médiation culturelle dans les musées. On apprend le rôle, sa raison d'être ou comment la médiation permet-elle l'accès à l'art ...
+      Ce livre traite de la médiation culturelle dans les musées. On apprend le rôle, sa raison d'être ou comment la médiation permet-elle l'accès à l'art...
     </p>
    
 
@@ -249,7 +249,7 @@
   </article>
 
 
-  <article class="filterDiv realitevirtuelle">
+  <article class="filterDiv realitevirtuelle Art experienceimmersive numerique musee interactif">
     <h3>
       PAUGAM, Sandra & AYATS, Charles (réalisateurs).
     </h3>
@@ -261,11 +261,10 @@
     </h4> 
     
     <h5>
-     Produit par les studios : Cinétévé Expérience et ARTE FRANCE, en partenariat avec Backlight
-Studio, et présenté lors du Taipei Film Festival du 05 juillet au 10 juillet 2018.
+     Produit par les studios&#8239;: Cinétévé Expérience et ARTE FRANCE, en partenariat avec Backlight Studio, et présenté lors du Taipei Film Festival du 05 juillet au 10 juillet 2018.
     </h5>
     
-    <blockquote cit="#">Êtes-vous prêt à libérer les secrets du « cri » le plus célèbre de l’histoire de l’art ?</blockquote>
+    <blockquote cit="#">Êtes-vous prêt à libérer les secrets du «&#8239;cri&#8239;» le plus célèbre de l’histoire de l’art&#8239;?</blockquote>
     
     <p> 
     Le Cri VR est une expérience VR Interactive en 3D temps réel jouable aux contrôleurs. Cette expérience, nous plonge à la découvertes des secrets que recèle la toile
@@ -273,7 +272,7 @@ et nous fait traverser les obsessions et l’œuvre du peintre. Découpée en tr
     </p>
    
 
-    <h6> 
+    <h6>  
      Consulté le 02 Octobre 2021
     </h6>
   
