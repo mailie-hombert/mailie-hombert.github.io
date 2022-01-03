338a3953419241ef7d1033b6164e17e42cda3f03
mailiehombert <67342315+mailiehombert@users.noreply.github.com>
2022-01-03 15:07:05 +0100

    modifications articles

--- INR_mailie/index.php
+++ INR_mailie/index.php
@@ -12,6 +12,11 @@
   require "articles.php";
   ?>
 
+<footer>
+  <p>Ces recherches se sont déroulées du 08 octobre au 09 décembre 2021. <br>
+  Typographies utilisées&#8239;: Jost et Georgia.</p>
+</footer>
+
 </body>
 
 </html>
